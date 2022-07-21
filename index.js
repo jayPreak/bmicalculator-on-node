8a046662566dc09d9e67904ad48464e08cdea91d
jayPreak <jayesh.preak2003@gmail.com>
2022-07-21 14:00:44 +0530

    hmm calculator works somehow

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + "/index.html")
 })
 
-app.get('/yep', (req, res) => {
-    res.send('Hello Worldgayge!')
-  })
+app.use(express.static(__dirname))
 
-app.get('/nop', (req, res) => {
-    res.send('notgaygeHello Worldgayge!')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
